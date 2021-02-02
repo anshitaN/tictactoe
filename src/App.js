512f338c3cb40f71732ab16986321396d4cd5be0
anshitaN <anshitanair2000@gmail.com>
2021-02-02 15:09:29 +0530

    added sass file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 /* eslint-disable arrow-body-style */
 import React from "react";
 import Board from './components/Board'
+import './Styles/root.scss'
 // import Square from './components/Square'
 
 const App = () => {
