49394e9ea0e524e8bbf8818d774e8f9125ddc554
anshitaN <anshitanair2000@gmail.com>
2021-02-02 16:14:09 +0530

    react events

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,23 +1,53 @@
-import React from 'react'
+/* eslint-disable arrow-body-style */
+import React, {useState} from 'react'
 import Square from './Square'
 // eslint-disable-next-line arrow-body-style
 const Board = () => {
+  const [board, setBoard]=useState(Array(9).fill(null));
+  const[isXNext, setIsXNext]=useState(false);
+  const handleSquareClick=(position)=>{
+    /*Not edit the X or O again if selected*/
+if(board[position]){
+  return;
+}
+//to select X or O
+    setBoard((prev)=>{
+      return prev.map((square, pos) =>{
+if(pos === position){
+  return isXNext? 'X':'O';
+}
+return square;
+      })
+    })
+    setIsXNext((prev)=>!prev);//to tap next either X or O according to pos
+  };
+
+const renderSquare=position=>{
+  return( 
+  <Square  
+    value={board[position]} 
+    onClick={()=>
+    handleSquareClick(position)}
+  />
+  );
+};
   return (
     <div className="board">
       <div className="board-row">
-       <Square  value={0}/>
-       <Square value={1}/>
-       <Square value={2}/>
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      
       </div>
       <div className="board-row">
-      <Square value={3}/>
-       <Square value={4}/>
-       <Square value={5}/>
+      {renderSquare(3)}
+      {renderSquare(4)}
+      {renderSquare(5)}
       </div>
       <div className="board-row">
-      <Square value={6}/>
-       <Square value={7}/>
-       <Square value={8}/> 
+      {renderSquare(6)}
+      {renderSquare(7)}
+      {renderSquare(8)}
       </div>
     </div>
   );
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,11 +1,16 @@
 /* eslint-disable arrow-body-style */
 import React from 'react';
 
-const Square = ({value}) => {
+const Square = ({value, onClick}) => {
 
-   
-      return  <button type="button" className="square">{value }</button>;
-  
+ return (
+        <button 
+        type="button" 
+        className="square" 
+      onClick={onClick}>
+            {value}
+      </button>
+  );
   
 };
 
