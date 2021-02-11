e25f954149f6a75a9923edb1d62292943dd276b9
anshitaN <anshitanair2000@gmail.com>
2021-02-11 11:12:40 +0530

    error resolved

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,20 +1,18 @@
 /* eslint-disable arrow-body-style */
 import React from 'react';
 
-const Square = ({value, onClick, isWinningSquare}) => {
-
- return (
-        <button 
-        type="button" 
-        
+const Square = ({ value, onClick, isWinningSquare }) => {
+  return (
+    <button
+      type="button"
       onClick={onClick}
-     className={`square${isWinningSquare ? 'winning' : ' '}${value==='X' ? 'text-green':'text-orange'}`}
-    
-      >
-            {value}
-      </button>
+      className={`square ${isWinningSquare ? 'winning' : ''} ${
+        value === 'X' ? 'text-green' : 'text-orange'
+      }`}
+    >
+      {value}
+    </button>
   );
-  
 };
 
 export default Square;
